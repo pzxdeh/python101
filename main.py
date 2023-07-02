89346743b8e72156ee6480b8db9cbe4cb05d9b16
PedramZadeh <pedramzadeh@outlook.com>
2023-07-02 20:21:21 +0100

    fix #1

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import time
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -13,6 +12,5 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    print('time is ->', time.time())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
