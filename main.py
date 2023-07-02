012b316b1041460113f326d3e8c930b40be18795
PedramZadeh <pedramzadeh@outlook.com>
2023-07-02 20:18:25 +0100

    print current time

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -12,5 +13,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print('time is ->', time.time())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
